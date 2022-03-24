b635a09c5e6147e1c962b0a9ae373ef998c2a609
Cristián Salinas <cristian@salin.as>
2022-03-24 16:39:13 -0300

    Refactorizando
    Actualizando el script para ordenarlo con clases antes de agregar más funcionalidades.

--- README.md
+++
@@ -1,2 +0,0 @@
-# NERV timer
- A clock based in the design of famous timer from Neon Genesis Evangelion
--- main.js
+++ main.js
@@ -1,74 +1,94 @@
-
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    document.getElementById('timeSession').src='src/AM-img.png';
-    document.getElementById("wrapper").classList.add('racing');
-    document.getElementById("wrapper").classList.remove('normal');
-    
-    // morning
-    if (h > 6) {
-        document.getElementById('timeSession').src='src/AM-img.png';
-        document.getElementById("wrapper").classList.add('normal-10');
-        document.getElementById("wrapper").classList.remove('racing');
-    }
-
-// noon
-    if (h >= 12) {
-        document.getElementById('timeSession').src='src/PM-img.png';
-        document.getElementById("wrapper").classList.add('normal-25');
-        document.getElementById("wrapper").classList.remove('racing');
-    }
-
-// afternoon
-if (h > 15) {
-    document.getElementById('timeSession').src='src/PM-img.png';
-    document.getElementById("wrapper").classList.add('normal-50');
-    document.getElementById("wrapper").classList.remove('racing');
-}
-// night
-    if (h > 17) {
-        document.getElementById('timeSession').src='src/PM-img.png';
-        document.getElementById("wrapper").classList.add('normal-75');
-        document.getElementById("wrapper").classList.remove('racing');
-    }
-// ultranight
-    if (h > 22) {
-        document.getElementById('timeSession').src='src/PM-img.png';
-        document.getElementById("wrapper").classList.add('racing');
-        document.getElementById("wrapper").classList.remove('normal');
-    }
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-    }
-
-    if(h > 6 && h < 12 ) {
-        
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    
-    // document.getElementById("bigClock").innerText = time;
-    // document.getElementById("bigClock").textContent = time;
-    document.getElementById("clockHour").innerText = h;
-    document.getElementById("clockHour").textContent = h;
-    document.getElementById("clockMinute").innerText = m;
-    document.getElementById("clockMinute").textContent = m;
-    document.getElementById("clockSecond").innerText = s;
-    document.getElementById("clockSecond").textContent = s;
-    
-    setTimeout(showTime, 1000);
-    
-}
-
-showTime();
+// Init Global Vars
+
+let nervClock;
+
+// Classes
+class Timer {
+    constructor() {
+        this.clockTicking = setInterval(() => {
+            let currentDate = new Date();
+            let hours = (currentDate.getHours() > 12 ? currentDate.getHours()-12 : currentDate.getHours());
+            hours = (hours < 10 ? '0' : '') + hours;
+            let minutes = (currentDate.getMinutes() < 10 ? '0' : '') + currentDate.getMinutes();
+            let seconds = (currentDate.getSeconds() < 10 ? '0' : '') + currentDate.getSeconds();
+            let time = hours+':'+minutes+':'+seconds;
+            //console.log(time);
+            if (!this.paused) {
+                document.getElementById("clockHour").innerText = hours;
+                document.getElementById("clockMinute").innerText = minutes;
+                document.getElementById("clockSecond").innerText = seconds;
+            }
+        }, 1000);
+        this.paused = false;
+    }
+    
+    pause() {
+        this.paused = true;
+    }
+    
+    start() {
+        this.paused = false;
+    }
+}
+
+class GUI extends Timer{
+    constructor() {
+        super();
+        this.level = 'normal-10';
+        this.meridian = 'AM';
+        this.mode = 'clock';
+    }
+    checkTime(what) {
+        let h = new Date();
+        h = h.getHours();
+        // morning - 06:00 to 14:59
+        if (what == 'level') {
+            if (h >= 6 && h < 15) {
+                return 'normal-10';
+            }
+    
+            // noon 25% - 15:00 to 16:59
+            if (h >= 15 && h < 17) {
+                return 'normal-25';
+            }
+    
+            // afternoon 50% - 17:00 to 19:59
+            if (h >= 17 && h < 20) {
+                return 'normal-50';
+            }
+            
+            // night 75% - 20:00 to 21:59
+            if (h >= 20 && h < 22) {
+                return 'normal-75';
+            }
+            
+            // ultranight racing! 100%!!
+            if (h >= 22 && h < 6) {
+                return 'racing';
+            }
+        }
+        if (what == 'meridian') {
+            if (h >= 12) {
+                return 'PM';
+            } else {
+                return 'AM';
+            }
+        }
+        
+    }
+    drawClock(level, meridian) {
+        let wrapper = document.getElementById("wrapper");
+        let timeSession = document.getElementById("timeSession");
+        wrapper.className = '';
+        wrapper.classList.add(level);
+        timeSession.src = `src/${meridian}-img.png`;
+        console.log(this.checkTime());
+    }
+
+}
+
+// Initialized after window loaded
+window.onload = function() {
+    nervClock = new GUI();
+    nervClock.drawClock(nervClock.checkTime('level'),nervClock.checkTime('meridian'));
+}
